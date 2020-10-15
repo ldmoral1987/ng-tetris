76df9a52e6ae46bf24e8cf63ae8f8648edc8d650
Luis del Moral Martínez <ldmoral1987>
2020-10-15 11:42:38 +0200

    Creación de la función de inicialización del tablero

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -10,32 +10,49 @@ import { COLS, BLOCK_SIZE, ROWS } from '../constants';
 })
 export class BoardComponent implements OnInit {
 
-   // Se captura la referencia del canvas (de la vista HTML)
-   // En el canvas se pintará el tablero de juego
-   @ViewChild('board', { static: true })
-   canvas: ElementRef<HTMLCanvasElement>;
+  // Se captura la referencia del canvas (de la vista HTML)
+  // En el canvas se pintará el tablero de juego
+  @ViewChild('board', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
  
-   ctx: CanvasRenderingContext2D;
-   points: number;
-   lines: number;
-   level: number;
+  ctx: CanvasRenderingContext2D;
+
+  // Variables del juego
+  points: number;
+  lines: number;
+  level: number;
+
+  // Tablero de juego
+  board: number[][];
  
-   ngOnInit() {
-     this.initBoard();
-   }
+  ngOnInit() {
+    this.initBoard();
+  }
  
-   // Inicialización del tablero
-   initBoard() {
-     // Se captura la referencia del contexto 2D en el que
-     // se dibujarán las piezas.
-     this.ctx = this.canvas.nativeElement.getContext('2d');
+  // Inicialización del tablero
+  initBoard() {
+    // Se captura la referencia del contexto 2D en el que
+    // se dibujarán las piezas.
+    this.ctx = this.canvas.nativeElement.getContext('2d');
  
-     // Se calcula el tamaño del canvas a partir de las 
-     // constantes del juego (constants.ts)
-     this.ctx.canvas.width = COLS * BLOCK_SIZE;
-     this.ctx.canvas.height = ROWS * BLOCK_SIZE;
-   }
+    // Se calcula el tamaño del canvas a partir de las 
+    // constantes del juego (constants.ts)
+    this.ctx.canvas.width = COLS * BLOCK_SIZE;
+    this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+  }
  
-   play() {}
+  // Función principal del juego
+  // Se lanza desde el botón de la vista
+  play() {
+    this.board = this.getEmptyBoard();
+    console.table(this.board);
+  }
+
+  // Esta función prepara el tablero para el juego
+  // Las celdas del tablero se representan con números
+  // El 0 indica que están vacías y el 1-7 representan un color
+  getEmptyBoard(): number[][] {
+    return Array.from({ length: ROWS }, () => Array(COLS).fill(0));
+  }
 
 }
